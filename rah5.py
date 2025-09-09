1fa14be4b67c0b7d52c8a8151d7dacff2347c1db
FaziSPB <szymbana@gmail.com>
2025-09-09 13:06:10 +0200

    list of marks

--- rah5.py
+++ rah5.py
@@ -1,11 +1,20 @@
-oceny = [4, 3, 2, 1, 5, 6 , 7, 8, 9]
-suma = sum(oceny)
-srednia = suma/len(oceny)
-print("srednia ocen to: ",srednia)
+oceny_uczniow = [
+    [5, 3, 2],
+    [1, 4, 6],
+    [2, 5, 4]
+]
+srednie_uczniow = []
+for i, uczen in enumerate(oceny_uczniow):
+    srednia = sum(uczen)/len(uczen)
+    srednie_uczniow.append(srednia)
+    print("Srednia ucznia", i+1, ":", srednia)
 
-powyzej = []
-for ocena in oceny:
-    if ocena > srednia:
-        powyzej.append(ocena)
-print("oceny powyzej sredniej to",powyzej)
+
+srednia_klasy = sum(srednie_uczniow)/len(srednie_uczniow)
+print("Srednia klasy to: ", srednia_klasy)
+
+print("Powyzej sredniej klasy: ")
+for k, srednia in enumerate(srednie_uczniow):
+    if srednia > srednia_klasy:
+            print("uczen ",k+1, ":", srednia)
 
