4343bc65f7a9b7be149c254acfdc963871661541
FaziSPB <szymbana@gmail.com>
2025-09-09 11:52:41 +0200

    simple math quiz

--- rah4.py
+++ rah4.py
@@ -1,16 +1,17 @@
 import random
-liczba = random.randint(1,10)
-for i in range(3):
-    liczba2 = int(input("Zgadnij liczbe: "))
+punkty = 0
+for i in range(5):
+    liczba1 = random.randint(1,10)
+    liczba2 = random.randint(1,10)
+    wynik = liczba1 * liczba2
+    odpowiedz = int(input(f"Ile to jest {liczba1}x{liczba2}?: "))
+    if odpowiedz == wynik:
+        punkty = punkty + 1
+        if i<5: print("Dobra odpowiedz, oto nastepne pytanie:")
+        else: print ("Dobra odpowiedz!")
+    else:
+        if i<5: print("Zla odpowiedz, oto nastepne pytanie:")
+        else: print ("Zla odpowiedz!")
+print("Oto zebrana liczba punktow:",punkty)
 
-    if liczba == liczba2:
-        print("Brawo! Odgadles!")
-        break
-    elif liczba2 > liczba:
-       if i < 2:
-            print("Za duza, Sprobuj jeszcze raz")
-    elif liczba2 < liczba:
-        if i < 2:
-            print("Za mala, Sprobuj jeszcze raz")
-else:
-    print("Nie znaleziono, poprawna liczba to", liczba)
+
